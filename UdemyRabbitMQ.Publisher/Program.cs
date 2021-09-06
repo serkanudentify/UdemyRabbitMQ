7047264cf11059cf7511c5dc4e794c0800e5c4c9
serkanudentify <86951594+serkanudentify@users.noreply.github.com>
2021-09-06 09:34:18 +0300

    Fanout-Exchange

--- UdemyRabbitMQ.Publisher/Program.cs
+++ UdemyRabbitMQ.Publisher/Program.cs
@@ -9,7 +9,6 @@ namespace UdemyRabbitMQ.Publisher
     {
         static void Main(string[] args)
         {
-            //Bir factory oluştur ve RabbitMq bağlantı URL'i ver
             var factory = new ConnectionFactory();
             factory.Uri = new Uri("amqps://dwfjvqxq:A-d4GvrxPdADiKBvgwAgOrCEfampaYAd@hornet.rmq.cloudamqp.com/dwfjvqxq");
 
@@ -17,20 +16,19 @@ namespace UdemyRabbitMQ.Publisher
             using var connection = factory.CreateConnection();
             var channel = connection.CreateModel();
 
-            // Kuyruk oluştur.
-            // queue: kuyruk ismi
-            // durable: Memory'de yada Fiziksel olarak tutulsun. false:'de memory'de tutulur, restart atarsan tüm kuyruk boşalır.)
-            // exclusive: true ise sadece yukarıda oluşturduğumuz kanal üzerinden iletişim kurulur. Subscriber farklı proje olduğundan false olacak.
-            // autoDelete: Kuyruğu dinleyen subscriber ile bağlantı koparsa kuyruk silinir.
-            channel.QueueDeclare("hello-queue", true, false, false);
+            //Fanout EXCHANGE
+            /*
+            Fanout Exchange'de kuyruğu publisher oluşturmaz. Kuyruğu dinleyen olmadığı sürece mesajlar boşa gider.
+            SendGrid bildirim yapısı gibi. Eğer consumer exchange'i dinlerse subscriber onu dinleyen consumer'ların kuyruğuna mesajı basar.
+            Saat başı hava tahmini dağıtan bir api olsun. Sadece bağlı siteler alabilir.
+            */
+            channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
 
             Enumerable.Range(1, 50).ToList().ForEach(x =>
             {
-                //Mesaj yaz ve byte'a çevir.(Byte gönderebiliriz!)
                 var messageBody = Encoding.UTF8.GetBytes($"Message {x}");
-                //Mesajı kuyruğa gönder.
-                channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
-                Console.WriteLine($"Message sent - {x}");
+                channel.BasicPublish("logs-fanout", string.Empty, null, messageBody);
+                Console.WriteLine($"Log sent - {x}");
             });
         }
     }
--- UdemyRabbitMQ.Subscriber/Program.cs
+++ UdemyRabbitMQ.Subscriber/Program.cs
@@ -8,7 +8,7 @@ namespace UdemyRabbitMQ.Subscriber
 {
     class Program
     {
-         static void Main(string[] args)
+        static void Main(string[] args)
         {
             //Bir factory oluştur ve RabbitMq bağlantı URL'i ver
             var factory = new ConnectionFactory();
@@ -18,22 +18,25 @@ namespace UdemyRabbitMQ.Subscriber
             using var connection = factory.CreateConnection();
             var channel = connection.CreateModel();
 
-            //kuyruğun olduğundan eminsen kaldır
-            //channel.QueueDeclare("hello-queue", true, false, false);
+            var randomQueueName = channel.QueueDeclare().QueueName;
 
-            //tek seferde kaç mesaj to subs
+            //queueDeclare kullanırsak subs kapanırsa bile kuyruk durur, ama queueBind kullanırsak mesaj gönderildikten sonra silinir.
+            channel.QueueBind(randomQueueName, "logs-fanout", string.Empty, null);
+
+            //Tek seferde kaç mesaj to subs
             channel.BasicQos(0, 1, false);
 
             var subscriber = new EventingBasicConsumer(channel);
 
             //false:  ise mesajı işledikten sonra silir, true ise mesajı alır almaz siler
-            channel.BasicConsume("hello-queue", false, subscriber);
+            channel.BasicConsume(randomQueueName, false, subscriber);
+            Console.WriteLine("We are listening the queue right now!");
 
             subscriber.Received += (object sender, BasicDeliverEventArgs e) =>
             {
                 var message = Encoding.UTF8.GetString(e.Body.ToArray());
                 Thread.Sleep(1000);
-                Console.WriteLine("Given message: " + message);
+                Console.WriteLine("A message from queue: " + message);
 
                 channel.BasicAck(e.DeliveryTag, false);
             };
