4cff7c263964956b17375eb4387951b20435c2ec
serkanudentify <86951594+serkanudentify@users.noreply.github.com>
2021-09-06 09:31:09 +0300

    Add project files.

---
+++ UdemyRabbitMQ.Publisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace UdemyRabbitMQ.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bir factory oluştur ve RabbitMq bağlantı URL'i ver
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://dwfjvqxq:A-d4GvrxPdADiKBvgwAgOrCEfampaYAd@hornet.rmq.cloudamqp.com/dwfjvqxq");
+
+            //Bir bağlantı aç ve channel oluştur.
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            // Kuyruk oluştur.
+            // queue: kuyruk ismi
+            // durable: Memory'de yada Fiziksel olarak tutulsun. false:'de memory'de tutulur, restart atarsan tüm kuyruk boşalır.)
+            // exclusive: true ise sadece yukarıda oluşturduğumuz kanal üzerinden iletişim kurulur. Subscriber farklı proje olduğundan false olacak.
+            // autoDelete: Kuyruğu dinleyen subscriber ile bağlantı koparsa kuyruk silinir.
+            channel.QueueDeclare("hello-queue", true, false, false);
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                //Mesaj yaz ve byte'a çevir.(Byte gönderebiliriz!)
+                var messageBody = Encoding.UTF8.GetBytes($"Message {x}");
+                //Mesajı kuyruğa gönder.
+                channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+                Console.WriteLine($"Message sent - {x}");
+            });
+        }
+    }
+}
---
+++ UdemyRabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace UdemyRabbitMQ.Subscriber
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            //Bir factory oluştur ve RabbitMq bağlantı URL'i ver
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://dwfjvqxq:A-d4GvrxPdADiKBvgwAgOrCEfampaYAd@hornet.rmq.cloudamqp.com/dwfjvqxq");
+
+            //Bir bağlantı aç ve channel oluştur.
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            //kuyruğun olduğundan eminsen kaldır
+            //channel.QueueDeclare("hello-queue", true, false, false);
+
+            //tek seferde kaç mesaj to subs
+            channel.BasicQos(0, 1, false);
+
+            var subscriber = new EventingBasicConsumer(channel);
+
+            //false:  ise mesajı işledikten sonra silir, true ise mesajı alır almaz siler
+            channel.BasicConsume("hello-queue", false, subscriber);
+
+            subscriber.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Thread.Sleep(1000);
+                Console.WriteLine("Given message: " + message);
+
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+
+            Console.ReadLine();
+        }
+    }
+}
